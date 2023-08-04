83216cb9dd4ac8d8833897801b659b5c1a7d6358
Drethetechgeek <Drethetechgeek@gmail.com>
2023-08-03 21:45:17 -0400

    created player input

--- rps.js
+++ rps.js
@@ -12,4 +12,26 @@ const getComputerChoice = () => {
         break;
     };
     }
-    // console.log(getComputerChoice());
+console.log(getComputerChoice());
+
+let getPlayerChoice = () => {
+    prompt("Rock, Paper, or Scissors: ").toLowerCase;
+}
+console.log(getPlayerChoice());
+
+/* function playRound(playerSelection,computerSelection) {
+    if (computerSelection === "rock" && playerSelection === "paper" || computerSelection === "scissors" && playerSelection === "rock" || computerSelection === "paper" && playerSelection === "scissors")
+    {return "Player has won!";
+}
+else if (computerSelection ==="scissors" && playerSelection ==="paper" || computerSelection ==="rock" && playerSelection ==="scissors" || computerSelection ==="paper" && playerSelection === "rock")
+    {return "You lose, Computer wins..";
+}
+else {return "Tie Game"
+}
+}; */
+
+
+
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+// console.log(playRound(playerSelection, computerSelection))
