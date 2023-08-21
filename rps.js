1caafd860bd9a2bcb5ccaaf65f5d7e8cfc057607
Drethetechgeek <Drethetechgeek@gmail.com>
2023-08-21 10:53:53 -0400

    fix get player round, add game func

--- rps.js
+++ rps.js
@@ -10,28 +10,60 @@ const getComputerChoice = () => {
         case 2:
         return "paper";
         break;
-    };
-    }
+}
+};
 console.log(getComputerChoice());
 
-let getPlayerChoice = () => {
-    prompt("Rock, Paper, or Scissors: ").toLowerCase;
-}
-console.log(getPlayerChoice());
-
-/* function playRound(playerSelection,computerSelection) {
-    if (computerSelection === "rock" && playerSelection === "paper" || computerSelection === "scissors" && playerSelection === "rock" || computerSelection === "paper" && playerSelection === "scissors")
-    {return "Player has won!";
-}
-else if (computerSelection ==="scissors" && playerSelection ==="paper" || computerSelection ==="rock" && playerSelection ==="scissors" || computerSelection ==="paper" && playerSelection === "rock")
-    {return "You lose, Computer wins..";
-}
-else {return "Tie Game"
-}
-}; */
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection === computerSelection){
+        return "Tie";
+    }
+    else if(
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")
 
 
+    ){
+        return "Player";
+    }
+    else{
+        return "Computer";
+    }
+}
 
-const computerSelection = getComputerChoice();
-const playerSelection = getPlayerChoice();
-// console.log(playRound(playerSelection, computerSelection))
+function playRound(playerSelection, computerSelection){
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result === "Tie"){
+        return "Tie Game"
+    }
+    else if (result === "Player"){
+        return `Player has won! ${playerSelection} beats ${computerSelection}`
+    }
+    else 
+    {
+        return `Computer wins, you lose... ${computerSelection} beats ${playerSelection}`
+    }
+    }
+
+    function getPlayerChoice(){
+        let validatedInput = false;
+        while(validatedInput = false){
+            const choice = prompt("Rock Paper Scissors");
+            if(choice == null) {
+                continue;
+            }
+        }
+    }
+
+    function game(){
+        console.log("Welcome!")
+        for(let i = 0; i < 5; i ++){
+            const playerSelection = "rock";
+            const computerSelection = getComputerChoice();
+            console.log(playRound(playerSelection, computerSelection));
+        }
+    }
+
+    game();
+
